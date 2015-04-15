5b1dcb2be489781aa365607879588819d1fff8ed
Maciej Stanik <M.Stanik@kainos.com>
2015-04-15 22:12:36 +0200

    Location app

--- Location/ViewController.h
+++ Location/ViewController.h
@@ -6,10 +6,15 @@
 //  Copyright (c) 2015 Maciej Stanik. All rights reserved.
 //
 
+#import <CoreLocation/CoreLocation.h>
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <CLLocationManagerDelegate>
 
+@property (weak, nonatomic) IBOutlet UILabel *latitudeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *longitudeLabel;
+
+- (IBAction)getLocation:(id)sender;
 
 @end
 
--- Location/ViewController.m
+++ Location/ViewController.m
@@ -8,7 +8,9 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    CLLocationManager *_locationManager;
+}
 
 @end
 
@@ -16,12 +18,42 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupLocationManager];
+    [_locationManager startUpdatingLocation];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)getLocation:(id)sender {
+    [_locationManager startUpdatingLocation];
+}
+
+#pragma mark Location methods
+
+- (void)setupLocationManager {
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    _locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    // Check for iOS 8. Without this guard the code will crash with "unknown selector" on iOS 7.
+    if ([_locationManager respondsToSelector:@selector(requestWhenInUseAuthorization)]) {
+        [_locationManager requestWhenInUseAuthorization];
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations {
+    CLLocation *currentLocation = [locations lastObject];
+    
+    if (currentLocation != nil) {
+        _longitudeLabel.text = [NSString stringWithFormat:@"Longitude: %.4f", currentLocation.coordinate.longitude];
+        _latitudeLabel.text = [NSString stringWithFormat:@"Latitude: %.4f:", currentLocation.coordinate.latitude];
+    }
+    [_locationManager stopUpdatingLocation];
+}
+
+- (void)locationManager:(CLLocationManager *)manager didFailWithError:(NSError *)error {
+    UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Application failed to get your location" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    [errorAlert show];
 }
 
 @end
